387d3792641440ad0a6e631bcfdc831f14e7a221
Hernandez <jordiahl@msl-100-7.lab.at.ufl.edu>
2017-04-02 18:34:10 -0400

    this is the playground project

---
+++ tutorial 1.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str:String = "Hello, playground";
+var jordi:Int=5;
+
+str.appendContentsOf((String(jordi)))
+
+var stringvalue="\(jordi)"
+
+print(str+stringvalue)
+
+
+
+
+
