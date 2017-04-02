fbe585a852d1f825d18aff356d243202760652b4
Hernandez <jordiahl@msl-100-7.lab.at.ufl.edu>
2017-04-02 19:17:53 -0400

    emoji tableView

--- Emoji Dictionary/Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/Emoji Dictionary/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var daCoolTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        daCoolTableView.dataSource = self;
+        daCoolTableView.delegate = self;
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell();
+        cell.textLabel?.text = "😂 😂 😂 😂 😂 😂 😂 😂 😂 😂😂 😂 😂 😂 😂 ";
+        return cell;
+    }
+    
 
+    
 
 }
 
